e1609982b1d9d4721ee007565765ae771ca762d5
Myra Anigbo <mca015@bucknell.edu>
2022-02-09 10:18:20 -0500

    adding nogood.c

---
+++ Labs/Lab04/dictionary.c
@@ -0,0 +1,39 @@
+/*
+ * Implements a dictionary's functionality
+ * <Your Name>
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+// Maximum number of words in the dictionary
+#define N 150000
+
+// array of dictionary words
+char words[N][LENGTH+1];
+
+// number of words in the dictionary
+int num_words = 0;
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    return false;
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary_filename)
+{
+    return true;
+}
+
+// Return the number of words stored in the dictionary.
+unsigned int size(void)
+{
+    return 0;
+
+}
---
+++ Labs/Lab04/sha_sum.c
@@ -0,0 +1,57 @@
+/*
+ * Compute the sha256 sum of a file
+ * <Your name>
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "sha256.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2){
+        // missing required file argument
+        printf("Usage %s FILE\n", argv[0]);
+        return(1);
+    }
+
+    SHA256_CTX hash;
+    const int BS = 1024;
+
+    unsigned char block[BS];
+
+    sha256_init(&hash);
+    
+    int f = open (argv[1], O_RDONLY);
+    if (f <  0){
+        perror("error");
+        return(2);
+    }
+
+    // TODO: Read from the file using the "read" function into "block"
+    // pass all read data to sha256_update
+    // repeat until you have processed the entire file
+
+    // Pseudo code:
+    // while ( read_block_from_file )
+    //     sha256_update(&hash, block, bytes_read)
+    
+    // YOUR CODE BEGINS HERE
+
+
+
+    // YOUR CODE ENDS HERE
+    // once you have read all of the file and passed it to sha256_update 
+    // we have to call sha256_final to ensure the hash algorithm has 
+    // processed all of the data.
+    sha256_final(&hash, block);
+
+    // now we can print the final hash, reusing the first SHA256_BLOCK_SIZE [32] bytes
+    // of the data block buffer to hold the final hash value.
+    for (int i=0; i < SHA256_BLOCK_SIZE; i++){
+        printf("%02x", block[i]);
+    }
+    printf("  %s\n", argv[1]);
+
+}
